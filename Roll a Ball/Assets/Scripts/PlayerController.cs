4785509c7dc06f75a97b451ebfaa161788a01d03
murock <alfie.page@btinternet.com>
2017-08-12 10:07:38 +0100

    Initial Upload of Work
    Initial Upload of Work

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    private List<GameObject> allPickUps;
+    private string buttonMovement;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        allPickUps = new List<GameObject>();
+        buttonMovement = "";
+    }
+
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("in key press down");
+            foreach ( GameObject pickUp in allPickUps)
+            {
+                Debug.Log("looking at object " + pickUp.name);
+                pickUp.SetActive (true);
+                count--;
+            }
+        }
+    }
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (buttonMovement == "down")
+        {
+            Debug.Log("Down");
+            moveVertical = -1;
+        } else if ( buttonMovement == "up")
+        {
+            Debug.Log("UP");
+            moveVertical = 1;
+        } else
+        {
+            moveVertical = Input.GetAxis("Vertical");
+        }
+
+        if (buttonMovement == "right")
+        {
+            moveHorizontal = 1;
+        } else if ( buttonMovement == "left")
+        {
+            moveHorizontal = -1;
+        }else
+        {
+            moveHorizontal = Input.GetAxis("Horizontal");
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+
+
+    }
+
+    public void updateMovement (string movement)
+    {
+        if(movement == "down")
+        {
+            buttonMovement = "down";
+        } else if(movement == "up")
+        {
+            buttonMovement = "up";
+        } else if(movement == "right")
+        {
+            buttonMovement = "right";
+        } else if(movement == "left")
+        {
+            buttonMovement = "left";
+        }
+    }
+
+    public void releasebutton ()
+    {
+        buttonMovement = "";
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            Debug.Log("Pick up is : " + other.gameObject.name);
+            allPickUps.Add(other.gameObject);
+            other.gameObject.SetActive (false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
---
+++ TowerDef_v2(pathing)/Assets/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LevelManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] tilePrefabs;
+
+    [SerializeField]
+    private CameraMovement cameraMovement;
+
+    private Dictionary<Point, TileScript> Tiles { get; set; }
+
+    private Point portalSpawn, coinSpawn;
+
+    [SerializeField]
+    private GameObject portalPrefab, coinPrefab;
+
+    public float TileSize
+    {
+        get {return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
+
+	// Use this for initialization
+	void Start () {
+        CreateLevel();
+        SpawnPortals();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void CreateLevel()
+    {
+        Tiles = new Dictionary<Point, TileScript>();
+
+        string[] mapData = ReadLevelText();
+
+        int mapX = mapData[0].ToCharArray().Length;
+        int mapY = mapData.Length;
+
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+
+        Vector3 maxTile = Vector3.zero;
+
+        for (int y = 0; y < mapY; y++)
+        {
+            char[] newTiles = mapData[y].ToCharArray();
+            for (int x = 0; x < mapX; x++)
+            {
+                PlaceTile(newTiles[x].ToString() ,x,y,worldStart);
+            }
+        }
+
+        maxTile = Tiles[new Point(mapX - 1, mapY - 1)].transform.position;
+
+        cameraMovement.SetLimits(new Vector3(maxTile.x + TileSize, maxTile.y - TileSize));
+    }
+
+    private void PlaceTile(string tileType,int x, int y, Vector3 worldStart)
+    {
+        int tileIndex = int.Parse(tileType);
+
+        TileScript newTile = Instantiate(tilePrefabs[tileIndex]).GetComponent<TileScript>();
+
+        newTile.Setup(new global::Point(x, y), new Vector3(worldStart.x + TileSize * x, worldStart.y - TileSize * y, 0));
+
+        Tiles.Add(new Point(x,y),newTile);
+
+    }
+
+    private string[] ReadLevelText()
+    {
+        TextAsset bindData = Resources.Load("Level") as TextAsset;
+
+        string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+
+        return data.Split('-');
+    }
+
+    private void SpawnPortals()
+    {
+        portalSpawn = new Point(0, 0);
+
+        Instantiate(portalPrefab, Tiles[portalSpawn].GetComponent<TileScript>().WorldPosition, Quaternion.identity);
+
+        coinSpawn = new Point(11, 6);
+
+        Instantiate(coinPrefab, Tiles[coinSpawn].GetComponent<TileScript>().WorldPosition, Quaternion.identity);
+    }
+}
