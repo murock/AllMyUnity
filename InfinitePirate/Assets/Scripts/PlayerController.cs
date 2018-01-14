e4b561767ffa2a3911f68777fa5c06285c041f63
murock <alfie.page@btinternet.com>
2018-01-14 09:43:18 +0000

    Updated so ship rotates

---
+++ InfinitePirate/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject playerShip;
+
+    [SerializeField]
+    private float playerSpeed = 3;
+
+    private bool rotatedRight, rotatedLeft;
+
+    // Use this for initialization
+    void Start()
+    {
+        rotatedRight = false;
+        rotatedLeft = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.rotationController();
+    }
+
+    private void rotateRight()
+    {
+        Vector3 rightAngle = new Vector3(0f, 0f, -45f);
+        if (!rotatedRight)
+        {
+            transform.Rotate(rightAngle, Space.World);
+            rotatedRight = true;
+        }
+    }
+
+    private void rotateLeft()
+    {
+        Vector3 leftAngle = new Vector3(0f, 0f, 45f);
+        if (!rotatedLeft)
+        {
+            transform.Rotate(leftAngle, Space.Self);
+            rotatedLeft = true;
+        }
+    }
+
+    private void rotationController()
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+        if (0 < direction)
+        {
+            if (rotatedLeft)   //Ensures left rotation is reset before rotating right
+            {
+                rotateRight();
+            }
+            else
+            {
+                this.rotateRight();
+                Vector3 diagonalRight = new Vector3(0.5f, 0.5f, 0);
+                transform.Translate(diagonalRight * playerSpeed * Time.deltaTime);
+            }
+        }
+        else if (0 > direction)
+        {
+            if (rotatedRight)
+            {
+                rotateLeft();
+            }
+            else
+            {
+                this.rotateLeft();
+                Vector3 diagonalLeft = new Vector3(-0.5f, 0.5f, 0);
+                transform.Translate(diagonalLeft * playerSpeed * Time.deltaTime);
+            }      
+        }
+        else
+        {
+            if (rotatedRight)
+            {
+                rotateLeft();
+            }
+            else if (rotatedLeft)
+            {
+                rotateRight();
+            }
+            rotatedLeft = false;
+            rotatedRight = false;
+        }
+    }
+}
