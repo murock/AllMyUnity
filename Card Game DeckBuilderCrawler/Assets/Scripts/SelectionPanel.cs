cdacd7f9ab14b84355b2d40611577eda77d473cc
murock <alfie.page@btinternet.com>
2018-09-12 20:09:52 +0100

    selectionpanel accept function

--- Card Game DeckBuilderCrawler/Assets/Scripts/SelectionPanel.cs
+++ Card Game DeckBuilderCrawler/Assets/Scripts/SelectionPanel.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectionPanel : MonoBehaviour {
+public class SelectionPanel : MonoBehaviour
+{
 
+    public void Accept()
+    {
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
 
+        }
+    }
 }
